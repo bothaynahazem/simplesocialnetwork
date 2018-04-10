1157b3d66e002c196e5b5c35bb31a689534fa230
Bothayna Hazem <bothayna.hazem@gmail.com>
2018-04-10 20:33:10 +0200

    the project folder

---
+++ simple_social_network_project/simple_social_network_project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_social_network_project
+{
+    class Program
+    {
+        public struct User
+        {
+
+        };
+
+        public struct Account
+        {
+
+        };
+
+        public struct Post
+        {
+
+        };
+
+        public struct Comment
+        {
+
+        };
+
+        public class SocialNetwork
+        {
+
+        };
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
